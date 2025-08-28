1467890ba4c51cfa8176fa310f3d90db60bac684
DamonJiang <jiang_kui@hoperun.com>
2025-08-28 17:13:15 +0800

    Merge pull request #9 from DemoJiang/br_rnoh0.77
    fix: 适配0.77，在RN侧定义组件属性时需要继承ViewProps

--- src/SpinKitNativeComponents.ts
+++ src/SpinKitNativeComponents.ts
@@ -1,10 +1,10 @@
 import {
-  HostComponent
+  HostComponent, ViewProps
 } from "react-native";
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { Int32, WithDefault } from 'react-native/Libraries/Types/CodegenTypes';
 
-export type SpinnerProps = {
+export interface SpinnerProps extends ViewProps {
   color?: WithDefault<string, '#000000'>;
   size?: WithDefault<Int32, 37>;
   type?: WithDefault<'CircleFlip'
